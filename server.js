028b64ddef63aa044741e4cee417237626d4d660
Talha Iqbal <talhaiqbal801@gmail.com>
2022-12-20 17:02:24 +0500

    Added global error handler

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
-require("dotenv").config();
-require("./database/connect")();
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+require('dotenv').config()
+require('./database/connect')()
+const errorHandler = require('./middleware/errorHandler')
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use("/api/v1/products", require("./routes/product"));
-app.use("/api/v1/users", require("./routes/user"));
+const PORT = process.env.PORT || 3000
+const app = express()
 
-app.get("/", (req, res, next) => {
-  res.send("Hello from server");
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/api/v1/products', require('./routes/product'))
+app.use('/api/v1/users', require('./routes/user'))
+app.use(errorHandler)
+
+app.get('/', (req, res, next) => {
+	res.send('Hello from server')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening at port ${PORT}`);
-});
-
-// error handling middleware
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-  res.status(500).send({
-    status: 500,
-    message: err.message,
-    body: {},
-  });
-});
+	console.log(`Server listening at port ${PORT}`)
+})
