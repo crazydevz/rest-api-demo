5b720d19df7182caa7306194fdda3ce6e486557e
Talha Iqbal <talhaiqbal801@gmail.com>
2023-02-07 00:12:44 +0500

    Added catchAsync wrapper

--- controllers/product.js
+++ controllers/product.js
@@ -2,20 +2,16 @@ const constants = require('../constants');
 const productService = require('../services/product');
 const catchAsync = require('../middleware/catchAsync');
 
-const createProduct = async (req, res) => {
+const createProduct = catchAsync(async (req, res) => {
 	let response = { ...constants.defaultServiceResponse };
-	try {
-		const payload = await productService.createProduct(req.body);
-		response.status = 200;
-		response.message = constants.productMessage.PRODUCT_CREATED;
-		response.body = payload;
-	} catch (err) {
-		console.log('Something went wrong: Controller: createProduct', err);
-		response.message = err.message;
-	}
+
+	const payload = await productService.createProduct(req.body);
+	response.status = 200;
+	response.message = constants.productMessage.PRODUCT_CREATED;
+	response.body = payload;
 
 	return res.status(response.status).send(response);
-};
+});
 
 const getAllProducts = catchAsync(async (req, res) => {
 	let response = { ...constants.defaultServiceResponse };
@@ -28,53 +24,44 @@ const getAllProducts = catchAsync(async (req, res) => {
 	return res.status(response.status).send(response);
 });
 
-const getProductById = async (req, res) => {
+const getProductById = catchAsync(async (req, res) => {
 	let response = { ...constants.defaultServiceResponse };
-	try {
-		const payload = await productService.getProductById(req.params);
-		response.status = 200;
-		response.message = constants.productMessage.PRODUCT_FETCHED;
-		response.body = payload;
-	} catch (err) {
-		console.log('Something went wrong: Controller: getProductById', err);
-		response.message = err.message;
-	}
 
+	const payload = await productService.getProductById(req.params.id);
+	response.status = 200;
+	response.message = constants.productMessage.PRODUCT_FETCHED;
+	response.body = payload;
+
+	// console.log('Something went wrong: Controller: getProductById', err);
 	return res.status(response.status).send(response);
-};
+});
 
-const updateProduct = async (req, res) => {
+const updateProduct = catchAsync(async (req, res) => {
 	let response = { ...constants.defaultServiceResponse };
-	try {
-		const payload = await productService.updateProduct({
-			id: req.params.id,
-			body: req.body,
-		});
-		response.status = 200;
-		response.message = constants.productMessage.PRODUCT_UPDATED;
-		response.body = payload;
-	} catch (err) {
-		console.log('Something went wrong: Controller: updateProduct', err);
-		response.message = err.message;
-	}
 
+	const payload = await productService.updateProduct({
+		id: req.params.id,
+		body: req.body,
+	});
+	response.status = 200;
+	response.message = constants.productMessage.PRODUCT_UPDATED;
+	response.body = payload;
+
+	// console.log('Something went wrong: Controller: updateProduct', err);
 	return res.status(response.status).send(response);
-};
+});
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = catchAsync(async (req, res) => {
 	let response = { ...constants.defaultServiceResponse };
-	try {
-		const payload = await productService.deleteProduct(req.params);
-		response.status = 200;
-		response.message = constants.productMessage.PRODUCT_DELETED;
-		response.body = payload;
-	} catch (err) {
-		console.log('Something went wrong: Controller: deleteProduct', err);
-		response.message = err.message;
-	}
 
+	const payload = await productService.deleteProduct(req.params);
+	response.status = 200;
+	response.message = constants.productMessage.PRODUCT_DELETED;
+	response.body = payload;
+
+	// console.log('Something went wrong: Controller: deleteProduct', err);
 	return res.status(response.status).send(response);
-};
+});
 
 module.exports = {
 	createProduct,
