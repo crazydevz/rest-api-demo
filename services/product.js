6df42e27d5a016e49b2f1b9d7753b5435cfeea75
Talha Iqbal <talhaiqbal801@gmail.com>
2023-02-07 00:12:17 +0500

    Removed unnecessary try catch

--- services/product.js
+++ services/product.js
@@ -3,70 +3,70 @@ const Product = require('../database/models/product');
 const { validateObjectId } = require('../helper/dbHelper');
 
 const createProduct = async data => {
-	try {
-		let product = new Product({ ...data });
-		let result = await product.save();
-		return result;
-	} catch (err) {
-		console.log('Something went wrong: Service: createProduct', err);
-		throw new Error(err);
-	}
+	// try {
+	let product = new Product({ ...data });
+	let result = await product.save();
+	return result;
+	// } catch (err) {
+	// 	console.log('Something went wrong: Service: createProduct', err);
+	// 	throw new Error(err);
+	// }
 };
 
 const getAllProducts = async ({ skip = 0, limit = 10 }) => {
-	try {
-		let products = await Product.find({})
-			.skip(parseInt(skip))
-			.limit(parseInt(limit));
-		return products;
-	} catch (err) {
-		console.log('Something went wrong: Service: getAllProducts', err);
-		throw new Error(err);
-	}
+	// try {
+	let products = await Product.find({})
+		.skip(parseInt(skip))
+		.limit(parseInt(limit));
+	return products;
+	// } catch (err) {
+	// 	console.log('Something went wrong: Service: getAllProducts', err);
+	// 	throw new Error(err);
+	// }
 };
 
-const getProductById = async ({ id }) => {
-	try {
-		validateObjectId(id);
-		let product = await Product.findById(id);
-		if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
-		return product;
-	} catch (err) {
-		console.log('Something went wrong: Service: getProductById', err);
-		throw new Error(err);
-	}
+const getProductById = async id => {
+	// try {
+	validateObjectId(id);
+	let product = await Product.findById(id);
+	if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+	return product;
+	// } catch (err) {
+	// 	console.log('Something went wrong: Service: getProductById', err);
+	// 	throw new Error(err);
+	// }
 };
 
 const updateProduct = async ({ id, body }) => {
-	try {
-		validateObjectId(id);
+	// try {
+	validateObjectId(id);
 
-		let conditions = {
-			_id: id,
-			body,
-		};
-		let update = body;
-		let options = { new: true };
+	let conditions = {
+		_id: id,
+		body,
+	};
+	let update = body;
+	let options = { new: true };
 
-		let product = await Product.findOneAndUpdate(conditions, update, options);
-		if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
-		return product;
-	} catch (err) {
-		console.log('Something went wrong: Service: updateProduct', err);
-		throw new Error(err);
-	}
+	let product = await Product.findOneAndUpdate(conditions, update, options);
+	if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+	return product;
+	// } catch (err) {
+	// 	console.log('Something went wrong: Service: updateProduct', err);
+	// 	throw new Error(err);
+	// }
 };
 
 const deleteProduct = async ({ id }) => {
-	try {
-		validateObjectId(id);
-		let product = await Product.findByIdAndDelete(id);
-		if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
-		return product;
-	} catch (err) {
-		console.log('Something went wrong: Service: deleteProduct', err);
-		throw new Error(err);
-	}
+	// try {
+	validateObjectId(id);
+	let product = await Product.findByIdAndDelete(id);
+	if (!product) throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+	return product;
+	// } catch (err) {
+	// 	console.log('Something went wrong: Service: deleteProduct', err);
+	// 	throw new Error(err);
+	// }
 };
 
 module.exports = {
